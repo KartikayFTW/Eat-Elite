e953b433ab08b96b8e29d15703994fc7cacbdf38
Kumar Kartikay <kartikaydwivedi55@gmail.com>
2024-03-15 00:17:59 +0530

    feat: initial commit for backend

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import { app } from "../app";
+import connectToDB from "./db";
+
+const PORT = process.env.PORT || 8000;
+connectToDB()
+  .then(() => {
+    const server = app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+
+    server.on("error", (error) => {
+      console.error("Server error:", error);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB CONNECTION_ERROR ", err);
+  });
