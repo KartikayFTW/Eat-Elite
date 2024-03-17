0cf5e6a2f8ebc2b764d2da80568c89ecff6f51ac
Kumar Kartikay <kartikaydwivedi55@gmail.com>
2024-03-17 23:57:08 +0530

    feat: auth0 implemenation with update user api

---
+++ backend/src/middleware/auth.ts
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+import { NextFunction, Request, Response } from 'express';
+import { auth } from 'express-oauth2-jwt-bearer';
+import jwt from 'jsonwebtoken';
+import User from 'models/user.models';
+
+const AUTH0_AUDIENCE = process.env.AUTH0_AUDIENCE;
+const AUTH0_BASE_URL = process.env.AUTH0_BASE_URL;
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string;
+      auth0Id: string;
+    }
+  }
+}
+
+export const jwtCheck = auth({
+  audience: AUTH0_AUDIENCE,
+  issuerBaseURL: AUTH0_BASE_URL,
+  tokenSigningAlg: 'RS256',
+});
+
+export const jwtParse = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return res.sendStatus(401);
+  }
+  const token = authorization.split(' ')[1];
+
+  try {
+    const decoded = jwt.decode(token) as jwt.JwtPayload;
+    const auth0Id = decoded.sub;
+    console.log('auth0Id', auth0Id);
+    const user = await User.findOne({
+      auth0Id,
+    });
+    if (!user) {
+      return res.sendStatus(401);
+    }
+    req.auth0Id = auth0Id as string;
+    req.userId = user._id.toString();
+    next();
+  } catch (error) {
+    console.log('error', error);
+    return res.sendStatus(401);
+  }
+};
