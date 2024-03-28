b34a73386c654bac045e0a65c3fd0bdee32402cb
Kumar Kartikay <kartikaydwivedi55@gmail.com>
2024-03-28 19:06:03 +0530

    feat: update restaurent api added

--- backend/src/controller/restaurantController.ts
+++ backend/src/controller/restaurantController.ts
@@ -23,7 +23,7 @@ const createRestaurant = async (req: Request, res: Response) => {
       imageUrl: uploadCloudinary.url,
       user: new mongoose.Types.ObjectId(req.userId),
     });
-    console.log('restaurant', restaurant);
+
     await restaurant.save();
 
     res.status(201).send(restaurant);
@@ -52,5 +52,42 @@ const getRestaurant = async (req: Request, res: Response) => {
     });
   }
 };
+const updateRestaurant = async (req: Request, res: Response) => {
+  try {
+    const existingRestaurant = await Restaurant.findOne({ user: req.userId });
+    if (!existingRestaurant) {
+      return res.status(404).json({
+        message: 'Restaurant not found',
+      });
+    }
 
-export { createRestaurant, getRestaurant };
+    let imageUrl = existingRestaurant.imageUrl;
+    if (req.file) {
+      const image = req.file as Express.Multer.File;
+      const base64Image = Buffer.from(image.buffer).toString('base64');
+      const dataUri = `data:${image.mimetype};base64,${base64Image}`;
+      const uploadCloudinary = await cloudinary.v2.uploader.upload(dataUri);
+      imageUrl = uploadCloudinary.url;
+    }
+
+    const updateData = {
+      ...req.body,
+      imageUrl: imageUrl,
+    };
+
+    await Restaurant.findByIdAndUpdate(existingRestaurant._id, updateData, {
+      new: true,
+    });
+
+    const updatedRestaurant = await Restaurant.findById(existingRestaurant._id);
+
+    res.status(200).json(updatedRestaurant);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: 'Error while updating the restaurant',
+    });
+  }
+};
+
+export { createRestaurant, getRestaurant, updateRestaurant };
